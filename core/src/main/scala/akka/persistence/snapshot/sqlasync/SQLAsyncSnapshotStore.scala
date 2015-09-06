040fad1f55482144ad0951217b5729ddb101f10e
okumin <mail@okumin.com>
2015-09-06 13:36:02 +0900

    Use `WITH` for upsert

--- core/src/main/scala/akka/persistence/snapshot/sqlasync/SQLAsyncSnapshotStore.scala
+++ core/src/main/scala/akka/persistence/snapshot/sqlasync/SQLAsyncSnapshotStore.scala
@@ -20,4 +20,18 @@ class MySQLSnapshotStore extends ScalikeJDBCSnapshotStore {
   }
 }
 
-class PostgreSQLSnapshotStore extends ScalikeJDBCSnapshotStore
+class PostgreSQLSnapshotStore extends ScalikeJDBCSnapshotStore {
+
+  import context.dispatcher
+
+  override protected[this] def upsert(persistenceId: String,
+                                      sequenceNr: Long,
+                                      timestamp: Long,
+                                      snapshot: Array[Byte]): Future[Unit] = {
+    sessionProvider.localTx { implicit session =>
+      val sql = sql"WITH upsert AS (UPDATE $table SET created_at = $timestamp, snapshot = $snapshot WHERE persistence_id = $persistenceId AND sequence_nr = $sequenceNr RETURNING *) INSERT INTO $table (persistence_id, sequence_nr, created_at, snapshot) SELECT $persistenceId, $sequenceNr, $timestamp, $snapshot WHERE NOT EXISTS (SELECT * FROM upsert)"
+      log.debug("Execute {}, binding persistence_id = {}, sequence_nr = {}, created_at = {}", sql.statement, persistenceId, sequenceNr, timestamp)
+      sql.update().future().map(_ => ())
+    }
+  }
+}
