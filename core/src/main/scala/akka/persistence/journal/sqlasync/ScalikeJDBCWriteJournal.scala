617b8ffd46fcbe857ea3502e3edb749b604596a5
okumin <mail@okumin.com>
2015-10-04 19:11:15 +0900

    Optimize persist

--- core/src/main/scala/akka/persistence/journal/sqlasync/ScalikeJDBCWriteJournal.scala
+++ core/src/main/scala/akka/persistence/journal/sqlasync/ScalikeJDBCWriteJournal.scala
@@ -43,38 +43,27 @@ private[sqlasync] trait ScalikeJDBCWriteJournal extends AsyncWriteJournal with A
         case (Failure(e), _) => Failure(e)
       }.map(_.reverse).map(batch.append)
     }
-    val highest = messages.foldLeft(Map.empty[String, Long]) { (acc, message) =>
-      val key = message.persistenceId
-      val value = acc.get(key) match {
-        case Some(x) => x max message.highestSequenceNr
-        case None => message.highestSequenceNr
-      }
-      acc.updated(key, value)
-    }
 
     val records = sqls.csv(batch: _*)
     val sql = sql"INSERT INTO $journalTable (persistence_id, sequence_nr, message) VALUES $records"
     log.debug("Execute {}, binding {}", sql.statement, messages)
     sessionProvider.localTx { implicit session =>
-      for {
-        _ <- sql.update().future()
-        _ <- highest.foldLeft(Future.successful(())) {
-          case (acc, (persistenceId, sequenceNr)) =>
-            for {
-              _ <- acc
-              _ <- updateSequenceNr(persistenceId, sequenceNr)
-            } yield ()
-        }
-      } yield result
+      sql.update().future().map(_ => result)
     }
   }
 
   override def asyncDeleteMessagesTo(persistenceId: String, toSequenceNr: Long): Future[Unit] = {
     log.debug("Delete messages, persistenceId = {}, toSequenceNr = {}", persistenceId, toSequenceNr)
+    val select = sql"SELECT sequence_nr FROM $journalTable WHERE persistence_id = $persistenceId ORDER BY sequence_nr DESC LIMIT 1"
+    log.debug("Execute {}, binding persistence_id = {} and sequence_nr = {}", select.statement, persistenceId, toSequenceNr)
+    val delete = sql"DELETE FROM $journalTable WHERE persistence_id = $persistenceId AND sequence_nr <= $toSequenceNr"
+    log.debug("Execute {}, binding persistence_id = {} and sequence_nr = {}", delete.statement, persistenceId, toSequenceNr)
     sessionProvider.localTx { implicit session =>
-      val sql = sql"DELETE FROM $journalTable WHERE persistence_id = $persistenceId AND sequence_nr <= $toSequenceNr"
-      log.debug("Execute {}, binding persistence_id = {} and sequence_nr = {}", sql.statement, persistenceId, toSequenceNr)
-      sql.update().future().map(_ => ())
+      for {
+        highest <- select.map(_.long("sequence_nr")).single().future().map(_.getOrElse(0L))
+        _ <- delete.update().future()
+        _ <- if (highest <= toSequenceNr) updateSequenceNr(persistenceId, highest) else Future.successful(())
+      } yield ()
     }
   }
 
@@ -95,12 +84,14 @@ private[sqlasync] trait ScalikeJDBCWriteJournal extends AsyncWriteJournal with A
   override def asyncReadHighestSequenceNr(persistenceId: String, fromSequenceNr: Long): Future[Long] = {
     log.debug("Read the highest sequence number, persistenceId = {}, fromSequenceNr = {}", persistenceId, fromSequenceNr)
     sessionProvider.withPool { implicit session =>
-      val sql = sql"SELECT sequence_nr FROM $persistenceIdTable WHERE persistence_id = $persistenceId"
-      log.debug("Execute {} binding persistence_id = {} and sequence_nr = {}", sql.statement, persistenceId, fromSequenceNr)
-      sql.map(_.long("sequence_nr")).single().future().map {
-        case Some(x) => x max fromSequenceNr
-        case None => fromSequenceNr
-      }
+      val fromPersistenceIdTable = sql"SELECT sequence_nr FROM $persistenceIdTable WHERE persistence_id = $persistenceId"
+      log.debug("Execute {} binding persistence_id = {} and sequence_nr = {}", fromPersistenceIdTable.statement, persistenceId, fromSequenceNr)
+      val fromJournalTable = sql"SELECT sequence_nr FROM $journalTable WHERE persistence_id = $persistenceId ORDER BY sequence_nr DESC LIMIT 1"
+      log.debug("Execute {} binding persistence_id = {} and sequence_nr = {}", fromJournalTable.statement, persistenceId, fromSequenceNr)
+      for {
+        p <- fromPersistenceIdTable.map(_.long("sequence_nr")).single().future().map(_.getOrElse(0L))
+        j <- fromJournalTable.map(_.long("sequence_nr")).single().future().map(_.getOrElse(0L))
+      } yield p max j max fromSequenceNr
     }
   }
 }
