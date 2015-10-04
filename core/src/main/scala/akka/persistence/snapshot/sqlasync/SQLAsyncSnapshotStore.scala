b14e7f72e478837b49516b46741498755c348155
okumin <mail@okumin.com>
2015-10-04 21:37:17 +0900

    Use PluginSettings on SnapshotStores

--- core/src/main/scala/akka/persistence/snapshot/sqlasync/SQLAsyncSnapshotStore.scala
+++ core/src/main/scala/akka/persistence/snapshot/sqlasync/SQLAsyncSnapshotStore.scala
@@ -1,37 +1,31 @@
 package akka.persistence.snapshot.sqlasync
 
+import akka.persistence.common.{MySQLPluginSettings, PostgreSQLPluginSettings}
 import scala.concurrent.Future
 import scalikejdbc._
 import scalikejdbc.async._
 
-class MySQLSnapshotStore extends ScalikeJDBCSnapshotStore {
-
-  import context.dispatcher
-
+class MySQLSnapshotStore extends ScalikeJDBCSnapshotStore with MySQLPluginSettings {
   override protected[this] def upsert(persistenceId: String,
                                       sequenceNr: Long,
                                       timestamp: Long,
                                       snapshot: Array[Byte]): Future[Unit] = {
     sessionProvider.localTx { implicit session =>
-      val sql = sql"INSERT INTO $table (persistence_id, sequence_nr, created_at, snapshot) VALUES ($persistenceId, $sequenceNr, $timestamp, $snapshot) ON DUPLICATE KEY UPDATE created_at = $timestamp, snapshot = $snapshot"
-      log.debug("Execute {}, binding persistence_id = {}, sequence_nr = {}, created_at = {}", sql.statement, persistenceId, sequenceNr, timestamp)
-      sql.update().future().map(_ => ())
+      val sql = sql"INSERT INTO $snapshotTable (persistence_id, sequence_nr, created_at, snapshot) VALUES ($persistenceId, $sequenceNr, $timestamp, $snapshot) ON DUPLICATE KEY UPDATE created_at = $timestamp, snapshot = $snapshot"
+      logging(sql).update().future().map(_ => ())
     }
   }
 }
 
-class PostgreSQLSnapshotStore extends ScalikeJDBCSnapshotStore {
-
-  import context.dispatcher
-
+class PostgreSQLSnapshotStore extends ScalikeJDBCSnapshotStore with PostgreSQLPluginSettings {
   override protected[this] def upsert(persistenceId: String,
                                       sequenceNr: Long,
                                       timestamp: Long,
                                       snapshot: Array[Byte]): Future[Unit] = {
     sessionProvider.localTx { implicit session =>
-      val sql = sql"WITH upsert AS (UPDATE $table SET created_at = $timestamp, snapshot = $snapshot WHERE persistence_id = $persistenceId AND sequence_nr = $sequenceNr RETURNING *) INSERT INTO $table (persistence_id, sequence_nr, created_at, snapshot) SELECT $persistenceId, $sequenceNr, $timestamp, $snapshot WHERE NOT EXISTS (SELECT * FROM upsert)"
+      val sql = sql"WITH upsert AS (UPDATE $snapshotTable SET created_at = $timestamp, snapshot = $snapshot WHERE persistence_id = $persistenceId AND sequence_nr = $sequenceNr RETURNING *) INSERT INTO $snapshotTable (persistence_id, sequence_nr, created_at, snapshot) SELECT $persistenceId, $sequenceNr, $timestamp, $snapshot WHERE NOT EXISTS (SELECT * FROM upsert)"
       log.debug("Execute {}, binding persistence_id = {}, sequence_nr = {}, created_at = {}", sql.statement, persistenceId, sequenceNr, timestamp)
-      sql.update().future().map(_ => ())
+      logging(sql).update().future().map(_ => ())
     }
   }
 }
--- core/src/main/scala/akka/persistence/snapshot/sqlasync/ScalikeJDBCSnapshotStore.scala
+++ core/src/main/scala/akka/persistence/snapshot/sqlasync/ScalikeJDBCSnapshotStore.scala
@@ -1,24 +1,16 @@
 package akka.persistence.snapshot.sqlasync
 
-import akka.actor.ActorLogging
 import akka.persistence._
-import akka.persistence.common.{ScalikeJDBCExtension, ScalikeJDBCSessionProvider}
+import akka.persistence.common.PluginSettings
 import akka.persistence.serialization.Snapshot
 import akka.persistence.snapshot.SnapshotStore
-import akka.serialization.{Serialization, SerializationExtension}
 import scala.concurrent.Future
 import scalikejdbc._
 import scalikejdbc.async._
 import scalikejdbc.interpolation.SQLSyntax
 
-private[persistence] trait ScalikeJDBCSnapshotStore extends SnapshotStore with ActorLogging {
-
-  import context.dispatcher
-
-  private[this] val serialization: Serialization = SerializationExtension(context.system)
-  private[this] lazy val extension: ScalikeJDBCExtension = ScalikeJDBCExtension(context.system)
-  protected[this] lazy val sessionProvider: ScalikeJDBCSessionProvider = extension.sessionProvider
-  protected[this] lazy val table = {
+private[persistence] trait ScalikeJDBCSnapshotStore extends SnapshotStore with PluginSettings {
+  protected[this] lazy val snapshotTable = {
     val tableName = extension.config.snapshotTableName
     SQLSyntaxSupportFeature.verifyTableName(tableName)
     SQLSyntax.createUnsafely(tableName)
@@ -28,9 +20,8 @@ private[persistence] trait ScalikeJDBCSnapshotStore extends SnapshotStore with A
     log.debug("Load a snapshot, persistenceId = {}, criteria = {}", persistenceId, criteria)
     sessionProvider.withPool { implicit session =>
       val SnapshotSelectionCriteria(maxSequenceNr, maxTimestamp, minSequenceNr, minTimestamp) = criteria
-      val sql = sql"SELECT * FROM $table WHERE persistence_id = $persistenceId AND sequence_nr <= $maxSequenceNr AND sequence_nr >= $minSequenceNr AND created_at <= $maxTimestamp AND created_at >= $minTimestamp ORDER BY sequence_nr DESC LIMIT 1"
-      log.debug("Execute {} binding persistence_id = {}, criteria = {}", sql.statement, persistenceId, criteria)
-      sql.map { result =>
+      val sql = sql"SELECT * FROM $snapshotTable WHERE persistence_id = $persistenceId AND sequence_nr <= $maxSequenceNr AND sequence_nr >= $minSequenceNr AND created_at <= $maxTimestamp AND created_at >= $minTimestamp ORDER BY sequence_nr DESC LIMIT 1"
+      logging(sql).map { result =>
         val Snapshot(snapshot) = serialization.deserialize(result.bytes("snapshot"), classOf[Snapshot]).get
         SelectedSnapshot(
           SnapshotMetadata(result.string("persistence_id"), result.long("sequence_nr"), result.long("created_at")),
@@ -54,9 +45,8 @@ private[persistence] trait ScalikeJDBCSnapshotStore extends SnapshotStore with A
     sessionProvider.localTx { implicit session =>
       // Ignores the timestamp since the target is specified by the sequence_nr.
       val SnapshotMetadata(persistenceId, sequenceNr, _) = metadata
-      val sql = sql"DELETE FROM $table WHERE persistence_id = $persistenceId AND sequence_nr = $sequenceNr"
-      log.debug("Execute {}, binding {}", sql.statement, metadata)
-      sql.update().future().map(_ => ())
+      val sql = sql"DELETE FROM $snapshotTable WHERE persistence_id = $persistenceId AND sequence_nr = $sequenceNr"
+      logging(sql).update().future().map(_ => ())
     }
   }
 
@@ -64,9 +54,8 @@ private[persistence] trait ScalikeJDBCSnapshotStore extends SnapshotStore with A
     log.debug("Delete the snapshot for {}, criteria = {}", persistenceId, criteria)
     sessionProvider.localTx { implicit session =>
       val SnapshotSelectionCriteria(maxSequenceNr, maxTimestamp, minSequenceNr, minTimestamp) = criteria
-      val sql = sql"DELETE FROM $table WHERE persistence_id = $persistenceId AND sequence_nr <= $maxSequenceNr AND sequence_nr >= $minSequenceNr AND created_at <= $maxTimestamp AND created_at >= $minTimestamp"
-      log.debug("Execute {}, binding persistence_id = {}, criteria = {}", sql.statement, persistenceId, criteria)
-      sql.update().future().map(_ => ())
+      val sql = sql"DELETE FROM $snapshotTable WHERE persistence_id = $persistenceId AND sequence_nr <= $maxSequenceNr AND sequence_nr >= $minSequenceNr AND created_at <= $maxTimestamp AND created_at >= $minTimestamp"
+      logging(sql).update().future().map(_ => ())
     }
   }
 }
